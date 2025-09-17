919df87e78b5ca6ae5600958d21728c9f5f7a970
Athar2022 <athar.hamady@gmail.com>
2025-09-17 23:56:16 +0300

    feat: update models

--- app/Models/Donation.php
+++ app/Models/Donation.php
@@ -2,9 +2,22 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Donation extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'donor_name',
+        'type',
+        'quantity',
+        'status',
+    ];
+
+    public function distributions()
+    {
+        return $this->hasMany(Distribution::class);
+    }
 }
